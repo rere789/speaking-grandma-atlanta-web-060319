7047ebbbc31a980ed51b3c03a10a9823b074274e
Reina Ewing <reina.castro3@gmail.com>
2019-05-16 14:56:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,6 @@ def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
-    return "HUH?! SPEAK UP, SUNNY"
+    return "HUH?! SPEAK UP, SUNNY!"
   end
 end
